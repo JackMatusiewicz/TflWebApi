2b65b071cf34d00eba5594d52f73236beba2d64d
JackMatusiewicz <jack.matusiewicz@gmail.com>
2017-11-15 07:48:30 +0000

    Adding basic query

--- site/webserver.fsx
+++ site/webserver.fsx
@@ -4,6 +4,10 @@
 
 #r "../packages/FAKE/tools/FakeLib.dll"
 #r "../packages/Suave/lib/net40/Suave.dll"
+#r "Newtonsoft.Json"
+#r "System.Net.Http.dll"
+#r "System.Configuration"
+#r "System.Data"
 
 open Fake
 open Suave
@@ -12,13 +16,25 @@ open Suave.Web
 open Suave.Types
 open System.Net
 
+open System
+open System.Net
+open System.Data
+open Newtonsoft.Json
+open System.Net.Http
+open System.Configuration;
+open System.Data.SqlClient;
+
+let getRows () =
+    let connectionString = ConfigurationManager.ConnectionStrings.["CarParkDbConnection"].ConnectionString
+    let con = new SqlConnection(connectionString)
+    con.Open()
+    let query = "SELECT COUNT(*) as Rows FROM dbo.CarParkStats"
+    use cmd = new SqlCommand(query, con)
+    let count = cmd.ExecuteScalar()
+    OK <| sprintf "Total rows: %s" (count.ToString())
+
 let serverConfig = 
     let port = getBuildParamOrDefault "port" "8083" |> Sockets.Port.Parse
     { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ] }
 
-startWebServer serverConfig 
-    (OK  
-        ("Hello World! It's Suave.io on Azure Websites.<br /><ul>" + 
-          "<li>Sample git Repo: <a href='https://github.com/shanselman/suavebootstrapper'>https://github.com/shanselman/suavebootstrapper</a></li>" +
-          "<li>Intro blog post: <a href='http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx'>http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx</a></li>" +
-          "</ul>"))
+startWebServer serverConfig (getRows ())
