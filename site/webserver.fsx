57f49cc9e44a808bf093a6b9cb98e380d8d933dc
JackMatusiewicz <jack.matusiewicz@gmail.com>
2017-11-15 19:34:08 +0000

    Reverting to original

--- site/webserver.fsx
+++ site/webserver.fsx
@@ -4,10 +4,10 @@
 
 #r "../packages/FAKE/tools/FakeLib.dll"
 #r "../packages/Suave/lib/net40/Suave.dll"
-#r "Newtonsoft.Json"
-#r "System.Net.Http.dll"
-#r "System.Configuration"
-#r "System.Data"
+//#r "Newtonsoft.Json"
+//#r "System.Net.Http.dll"
+//#r "System.Configuration"
+//#r "System.Data"
 
 open Fake
 open Suave
@@ -16,29 +16,16 @@ open Suave.Web
 open Suave.Types
 open System.Net
 
-open System
+(*open System
 open System.Net
 open System.Data
 open Newtonsoft.Json
 open System.Net.Http
 open System.Configuration;
-open System.Data.SqlClient;
-
-let getRows () =
-    try
-        //let connectionString = ConfigurationManager.ConnectionStrings.["CarParkDbConnection"].ConnectionString
-        //let con = new SqlConnection(connectionString)
-        //con.Open()
-        //let query = "SELECT COUNT(*) as Rows FROM dbo.CarParkStats"
-        //use cmd = new SqlCommand(query, con)
-        //let count = cmd.ExecuteScalar()
-        //OK <| sprintf "Total rows: %s" (count.ToString())
-        OK <| sprintf "Hello!"
-    with
-        | ex -> OK |< (ex.ToString())
+open System.Data.SqlClient;*)
 
 let serverConfig = 
     let port = getBuildParamOrDefault "port" "8083" |> Sockets.Port.Parse
     { defaultConfig with bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ] }
 
-startWebServer serverConfig (getRows ())
+startWebServer serverConfig (OK "TEST!")
