689de27c7b7d5b9de87c92b73f966dd4c580db68
JackMatusiewicz <jack.matusiewicz@gmail.com>
2017-11-18 12:47:23 +0000

    Adding command to query data between dates

--- site/webserver.fsx
+++ site/webserver.fsx
@@ -3,6 +3,7 @@
 // --------------------------------------------------------------------------------------
 
 #r "../packages/FAKE/tools/FakeLib.dll"
+open System.Windows.Forms
 #r "../packages/Suave/lib/net40/Suave.dll"
 #r "Newtonsoft.Json"
 #r "System.Net.Http.dll"
@@ -18,7 +19,6 @@ open Suave.Http
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
-//open Suave.Utils.Collections
 
 open Newtonsoft.Json
 
@@ -31,17 +31,61 @@ open System.Data.SqlClient;
 
 type Rows = {TotalRows : int}
 
+type CarParkDatabaseRecord = {
+    bayCount : uint32
+    free : uint32
+    occupied : uint32
+    bayType : string
+    carParkId : string
+    timestamp : System.DateTime
+}
+
+let constructSqlParameter (name : string) (paramType : SqlDbType) (value : obj) =
+    let p = SqlParameter(name, paramType)
+    p.Value <- value
+    p
+
+let getOpenConnection () =
+    let connectionString = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_CarParkDbConnection");
+    let con = new SqlConnection(connectionString)
+    con.Open()
+    con
+
 let getRows () = 
     try
-        let connectionString = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_CarParkDbConnection");
-        let con = new SqlConnection(connectionString)
-        con.Open()
+        use  con = getOpenConnection ()
         let query = "SELECT COUNT(*) as Rows FROM dbo.CarParkStats"
         use cmd = new SqlCommand(query, con)
         let count = cmd.ExecuteScalar() :?> int
         JsonConvert.SerializeObject({TotalRows = count})
     with
-        | ex -> sprintf "%s" (ex.ToString())    
+        | ex -> sprintf "%s" (ex.ToString())
+
+let constructRecord (reader : SqlDataReader) = 
+    {
+        bayCount = (uint32 (reader.GetInt32(reader.GetOrdinal("BayCount"))))
+        free = (uint32 (reader.GetInt32(reader.GetOrdinal("FreeSpaces"))))
+        occupied = (uint32 (reader.GetInt32(reader.GetOrdinal("OccupiedSpaces"))))
+        bayType = reader.GetString(reader.GetOrdinal("BayType"))
+        carParkId = reader.GetString(reader.GetOrdinal("CarParkId"))
+        timestamp = reader.GetDateTime(reader.GetOrdinal("InsertTime"))
+    }
+
+let getData (f : string) (t : string) : CarParkDatabaseRecord list =
+    let fromDate = DateTime.Parse(f)
+    let toDate = DateTime.Parse(t)
+    use  con = getOpenConnection ()
+    let query = "SELECT TOP 5 * FROM dbo.CarParkStats a WHERE a.InsertTime >= @from AND a.InsertTime <= @to"
+    use cmd = new SqlCommand(query, con)
+    cmd.Parameters.Add(constructSqlParameter "@from" (SqlDbType.DateTime2) fromDate) |> ignore
+    cmd.Parameters.Add(constructSqlParameter "@to" (SqlDbType.DateTime2) toDate) |> ignore
+    let reader = cmd.ExecuteReader()
+    if reader.HasRows then
+        [
+            while reader.Read()
+                do yield constructRecord reader
+        ]
+    else []    
 
 let serverConfig = 
     let port = getBuildParamOrDefault "port" "8083" |> Sockets.Port.Parse
@@ -49,6 +93,7 @@ let serverConfig =
 
 let app = choose [
             GET >=> path "/rows" >=> request (fun _ -> OK <| getRows ()) >=> Writers.setMimeType "application/json; charset=utf-8"
+            pathScan "data/from/%s/to/%s" getData
         ]
 
 startWebServer serverConfig app
